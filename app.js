fc397eba5f03570d6d51fff16014fd68bd412cf1
AdhamWafy <144845319+AdhamWafy@users.noreply.github.com>
2024-03-11 17:19:32 +0200

    Update "Missing Files Probable"

--- app.js
+++ app.js
@@ -12,3 +12,33 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenObjects = document.querySelectorAll(".hidden");
 hiddenObjects.forEach((el) => observer.observe(el));
+
+const observerTextLeft = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log("intersect")
+        if (entry.isIntersecting){
+            entry.target.classList.add("left-part-show");
+        }
+        else{
+            entry.target.classList.remove("left-part-show");
+        }
+    });
+})
+
+const hiddenObjectsTextLeft = document.querySelectorAll(".left-part-hide");
+hiddenObjectsTextLeft.forEach((el) => observerTextLeft.observe(el));
+
+const observerTextRight = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log("intersect")
+        if (entry.isIntersecting){
+            entry.target.classList.add("right-part-show");
+        }
+        else{
+            entry.target.classList.remove("right-part-show");
+        }
+    });
+})
+
+const hiddenObjectsTextRight = document.querySelectorAll(".right-part-hide");
+hiddenObjectsTextRight.forEach((el) => observerTextRight.observe(el));
