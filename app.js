5766a9fc820e97979658d1f01f42ab968cc82f74
AdhamWafy <144845319+AdhamWafy@users.noreply.github.com>
2024-03-13 22:36:09 +0200

    Dark Mooood!

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log("intersect")
-        if (entry.isIntersecting){
-            entry.target.classList.add("show");
-        }
-        else{
-            entry.target.classList.remove("show");
-        }
-    });
-})
-
-const hiddenObjects = document.querySelectorAll(".hidden");
-hiddenObjects.forEach((el) => observer.observe(el));
-
+//Detect if on screen
 const observerTextLeft = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log("intersect")
@@ -27,18 +13,17 @@ const observerTextLeft = new IntersectionObserver((entries) => {
 
 const hiddenObjectsTextLeft = document.querySelectorAll(".left-part-hide");
 hiddenObjectsTextLeft.forEach((el) => observerTextLeft.observe(el));
+//Switch Function
 
-const observerTextRight = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log("intersect")
-        if (entry.isIntersecting){
-            entry.target.classList.add("right-part-show");
-        }
-        else{
-            entry.target.classList.remove("right-part-show");
-        }
-    });
-})
-
-const hiddenObjectsTextRight = document.querySelectorAll(".right-part-hide");
-hiddenObjectsTextRight.forEach((el) => observerTextRight.observe(el));
+function Switch(){
+    body = document.getElementById("body");
+    if (body.classList.contains("dark"))
+    {
+        body.classList.remove("dark")
+        body.classList.add("root")
+    }
+    else{
+        body.classList.add("dark")
+        body.classList.remove("root")
+    }
+}
