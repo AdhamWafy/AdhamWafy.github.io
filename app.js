793db7cbc024f938703d2d58acb5d15bb5236743
AdhamWafy <144845319+AdhamWafy@users.noreply.github.com>
2024-03-15 19:51:52 +0200

    Des-2 is main now

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ const observerTextLeft = new IntersectionObserver((entries) => {
 
 const hiddenObjectsTextLeft = document.querySelectorAll(".left-part-hide");
 hiddenObjectsTextLeft.forEach((el) => observerTextLeft.observe(el));
+
+
+
+const observerTextRight = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log("intersect")
+        if (entry.isIntersecting){
+            entry.target.classList.add("right-part-show");
+        }
+        else{
+            entry.target.classList.remove("right-part-show");
+        }
+    });
+})
+
+const hiddenObjectsTextRight = document.querySelectorAll(".right-part-hide");
+hiddenObjectsTextRight.forEach((el) => observerTextRight.observe(el));
+
 //Switch Function
 
 function Switch(){
